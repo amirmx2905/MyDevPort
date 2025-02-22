bf7389ea80f20a73fcb7a332152d796e16fc8997
amirmx2905 <amir.flores.cardona@gmail.com>
2025-02-22 14:30:08 -0600

    ADDED a functionality for the buttons of the navegation bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,23 +2,29 @@ document.addEventListener('DOMContentLoaded', function() {
     const navLinks = document.querySelectorAll('.links a');
     const checkbox = document.getElementById('sidebar--active');
     const mainContent = document.querySelector('.main__content');
+    
+    const setActiveLink = (clickedLink) => {
+        navLinks.forEach(link => {
+            link.classList.remove('pressed');
+        });
+        clickedLink.classList.add('pressed');
+    };
+
+    const aboutMeLink = document.getElementById('aboutMe');
+    setActiveLink(aboutMeLink);
 
     navLinks.forEach(link => {
         link.addEventListener('click', async (e) => {
             e.preventDefault();
             
-            // Cierra el sidebar
             checkbox.checked = false;
-
-            // Fade out
+            
+            setActiveLink(e.target);
+            
             mainContent.classList.add('fade-out');
             
-            // Espera a que termine la animación
             await new Promise(resolve => setTimeout(resolve, 300));
-            
-            // Aquí puedes cambiar el contenido si lo deseas
-            
-            // Fade in
+                        
             mainContent.classList.remove('fade-out');
         });
     });
