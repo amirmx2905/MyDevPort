24b8e52009509be37b97457603a4e91087831df8
amirmx2905 <amir.flores.cardona@gmail.com>
2026-06-01 22:51:00 -0600

    INSTALLED shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { Button } from '@/components/ui/button'
+
 function App() {
   return (
-    <>
-      <div className="text-3xl font-bold underline">Hi</div>
-    </>
-  );
+    <main className="min-h-screen grid place-items-center p-6">
+      <div className="flex flex-col items-center gap-4">
+        <h1 className="text-3xl font-bold">Shadcn Button Test</h1>
+        <Button>Click me</Button>
+      </div>
+    </main>
+  )
 }
 
 export default App
